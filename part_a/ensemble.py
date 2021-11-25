e2a58cf6f3a45aa58562491802e6d1eac0893240
ruyiq <ruyicassiel@gmail.com>
2021-11-25 16:42:14 -0500

    update

--- part_a/ensemble.py
+++ part_a/ensemble.py
@@ -1,7 +1,7 @@
 # TODO: complete this file.
-from knn import *
+from utils import *
 from item_response import *
-from neural_network import *
+import torch
 
 def sample(train):
     '''
@@ -17,13 +17,81 @@ def sample(train):
 
     num_entries = len(id)
 
-    #Generates a random sample 
+    #draw a sample with replacement with the chosen size
     resample= np.random.choice(num_entries, num_entries, replace=True)
     
     return {"user_id": id[resample],
             "question_id": q[resample],
             "is_correct": is_correct[resample]}
 
-def evaluate():
-    pass
-# Test
+def ensemble_evaluate(data, theta, beta):
+    '''
+    For each bootstrap sample, estimate the skill of the model, calculate the mean of the sample of model skill estimates,
+    and then return the accuracy.
+    '''
+    pred1 = []
+    for i, q in enumerate(data["question_id"]):
+        pred2 = []
+        id = data["user_id"][i]
+        for k in range(len(theta)):
+            pred2.append(sigmoid((theta[k][id] - beta[k][q]).sum()))
+        pred1.append(np.mean(pred2) >= 0.5)
+    denominator = len(data["is_correct"])
+    numerator = np.sum((data["is_correct"] == np.array(pred1)))
+    return  numerator / denominator
+        
+
+if __name__ == '__main__':
+    train = load_train_csv("../data")
+    val = load_valid_csv("../data")
+    test = load_public_test_csv("../data")
+
+    # Select and train 3 base models with bootstrapping the training set.
+    print("-----Training the first model with bootstrap-----")
+    theta_1, beta_1, val_acc_lst_1, train_likelihood_1, val_likelihood_1 \
+        = irt(sample(train), val, 0.01, 50)
+
+    print()
+
+    print("-----Training the second model with bootstrap-----")
+    theta_2, beta_2, val_acc_lst_2, train_likelihood_2, val_likelihood_2 \
+        = irt(sample(train), val, 0.01, 50)
+
+    print()
+
+    print("-----Training the third model with bootstrap-----")
+    theta_3, beta_3, val_acc_lst_3, train_likelihood_3, val_likelihood_3 \
+        = irt(sample(train), val, 0.01, 50)
+
+    print()
+
+    theta = [theta_1, theta_2, theta_3]
+    beta = [beta_1, beta_2, beta_3]
+
+    # To predict the correctness, generate 3 predictions by using the base model and average the predicted correctness.
+
+    print("-----Evaluating each individual base model-----")
+    
+    print()
+    print()
+
+    print("Validation accuracy for each individual base model are:", 
+    evaluate(val, theta_1, beta_1),
+    evaluate(val, theta_2, beta_2), 'and',
+    evaluate(val, theta_3, beta_3)) 
+    
+    print("Test accuracy for each individual base model are:", 
+    evaluate(test, theta_1, beta_1),
+    evaluate(test, theta_2, beta_2), 'and',
+    evaluate(test, theta_3, beta_3)) 
+
+    print()
+    
+    print("-----Report final accuracy-----")
+
+    print()
+    print()
+    final_val_accuracy = ensemble_evaluate(val, theta, beta)
+    final_test_accuracy = ensemble_evaluate(test, theta, beta)
+    print("Final validation accuracy is: ", final_val_accuracy)
+    print("Final test accuracy is: ", final_test_accuracy)
--- part_a/neural_network.py
+++ part_a/neural_network.py
@@ -1,14 +1,12 @@
 from utils import *
+import torch
 from torch.autograd import Variable
 
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import torch.utils.data
-
 import numpy as np
-import torch
-
 import matplotlib.pyplot as plt
 
 
